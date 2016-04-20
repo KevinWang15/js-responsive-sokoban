62e2242ee22abefa6462b557be9dd8654af77ca4
Kevin Wang <453587854@qq.com>
2016-04-20 17:12:56 +0800

    eliminate duplicate levels

--- tools/level-converter/converter.js
+++ tools/level-converter/converter.js
@@ -20,24 +20,61 @@
  Where o stands for box, # stands for wall, . stands for target, @ stands for person
  To levels.js which this game can recognize.
 
+ will search data/ directory recursively
+
  */
 
-var max_map_size = 20;
+var max_map_size = 18;
 
-
+var hash = require('object-hash');
 var fs = require("fs");
 var path = require("path");
 var levels = [];
+var levelHashes = [];
+
+var dotCount = 0;
+var duplicateLevelCount = 0;
+var skippedLevelCount = 0;
+
+function makeLevelHash(level) {
+    return hash(level);
+}
+
+function random(low, high) {
+    return Math.floor(Math.random() * (high - low)) + low;
+}
 
 function makeLevel(fContent) {
     if (!fContent) return;
+
+    process.stdout.write("\033[3" + random(0, 9) + "m\033[4" + random(0, 9) + "m" + ["★", "○", "√", "÷", "×", "©", "￥"][random(0, 5)]);
+    dotCount++;
+    if (dotCount % 32 == 0)
+        process.stdout.write("\033[0m\n");
+
+
+    //remove trailing \n
+    if (fContent.charCodeAt(fContent.length - 1) == 10)
+        fContent = fContent.substr(0, fContent.length - 1);
+
+    //o may represent $ in some map formats
+    fContent = fContent.replace(/o/g, "$");
+
     var lines = fContent.split("\n");
     var height = lines.length;
     var width = -1;
+
+    var map_coords = {top: 10000, left: 10000, bottom: -10000, right: -10000};
+
     lines.forEach(function (d) {
         if (d.length > width) width = d.length;
     });
-    if (width > max_map_size || height > max_map_size) return;
+
+    if (width > max_map_size || height > max_map_size) {
+        skippedLevelCount++;
+        return;
+    }
+
     var level = {map: [], person: {}, box: []};
     for (var y = 0; y < height; y++) {
         var map_line = [];
@@ -48,17 +85,46 @@ function makeLevel(fContent) {
             } else {
                 char = " ";
             }
-            //o$: box, .: target, #: wall, *: box on target, +: person on target, @: person
+            if (char != " ") {
+                if (map_coords.left > x) map_coords.left = x;
+                if (map_coords.right < x) map_coords.right = x;
+                if (map_coords.top > y) map_coords.top = y;
+                if (map_coords.bottom < y) map_coords.bottom = y;
+            }
+            //$: box, .: target, #: wall, *: box on target, +: person on target, @: person
             map_line.push({' ': 0, '#': 1, 'o': 0, '@': 0, '.': 2, '$': 0, '+': 2, '*': 2}[char]);
             if (char == "@" || char == "+")
                 level.person = {x: x, y: y};
-            if (char == 'o' || char == '$' || char == '*') {
+            if (char == '$' || char == '*') {
                 level.box.push({x: x, y: y});
             }
         }
         level.map.push(map_line);
     }
-    levels.push(level);
+
+    if (map_coords.top != 0 || map_coords.left != 0 ||
+        map_coords.bottom != height - 1 || map_coords.right != width - 1) {
+        //resize map
+        var newMap = [];
+        for (y = map_coords.top; y <= map_coords.bottom; y++)
+            newMap.push(level.map[y].splice(map_coords.left, map_coords.right - map_coords.left + 1));
+        level.map = newMap;
+        level.person.x -= map_coords.left;
+        level.person.y -= map_coords.top;
+        level.box.forEach(function (d) {
+            d.x -= map_coords.left;
+            d.y -= map_coords.top;
+        });
+    }
+
+    var levelHash = makeLevelHash(level);
+    if (levelHashes.indexOf(levelHash) == -1) {
+        levelHashes.push(levelHash);
+        levels.push(level);
+    } else {
+        duplicateLevelCount++;
+    }
+
 }
 
 function splitLevels(fContent) {
@@ -74,6 +140,10 @@ function readDir(dirPath) {
             readDir(file);
         } else {
             var fContent = fs.readFileSync(file, {encoding: "utf8"});
+
+            //get rid of carriage returns so that all line-breaks will be \n
+            fContent = fContent.replace(/\r/g, "");
+
             splitLevels(fContent).forEach(function (f) {
                 makeLevel(f);
                 //console.log(f);
@@ -82,9 +152,20 @@ function readDir(dirPath) {
     }
 }
 
+console.log("\033[32mWorking on it\n");
+
+var time_start=+new Date();
+
 readDir("data");
 
-console.log(levels.length + " levels created");
+var time_end=+new Date();
+
+console.log("\033[0m\n\n\033[33m" + levels.length + " level" + (levels.length > 1 ? "s" : "") + " created, "
+    + duplicateLevelCount + " duplicate level" + (duplicateLevelCount > 1 ? "s" : "") + ", "
+    + skippedLevelCount + " skipped level" + (skippedLevelCount > 1 ? "s (because they are too big)" : "")
+);
+
+console.log("\n\033[36mFinished in "+(time_end-time_start)+" ms");
 fs.writeFileSync("levels.js",
     "//0:aisle\n//1:wall\n//2:target\nvar levelsData =" +
     JSON.stringify(levels)
