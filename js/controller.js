a90647b4386f3de5ef8468f76b81fdefce4f16ba
Kevin Wang <453587854@qq.com>
2016-04-20 15:20:36 +0800

    shuffle stages

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,15 @@
+//Shuffle levels
+
+if (!Array.prototype.shuffle) {
+    Array.prototype.shuffle = function () {
+        for (var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
+        return this;
+    };
+}
+
+levelsData.shuffle();
+
+
 var game = {
     currentStage: 0
 };
